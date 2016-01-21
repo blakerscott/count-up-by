b038b772d4ea4f5d95c10fb4134a92e789d18cb8
Blake Scott and Jay Whang <jaywhang83@gmail.com>
2016-01-21 10:35:21 -0800

    Create passing spec for multiples of 5

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,19 @@
-describe('counter', function() {
-  it("if number is divisible by 5 or 7", function() {
+describe('by5', function() {
+
+  it("if number is  not divisible by 5 or 7", function() {
     expect(counter(13)).to.equal(false);
   });
+  
+  it("if number is divisible by 5", function() {
+    expect(counter(10)).to.equal(true);
+  });
+
+  it("if number is divisible by 7", function() {
+    expect(counter(30)).to.equal(true);
+  });
+
+  it("if countTo is equal to 30 and countBy 5", function() {
+    expect(by5(5, 30)).to.equal(5, 10, 15, 20, 25, 30);
+  });
+
 });
