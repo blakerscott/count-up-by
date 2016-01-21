14aaae0059b1eb834370024d6ed74980c956b9a6
Blake Scott and Jay Whang <jaywhang83@gmail.com>
2016-01-21 11:49:17 -0800

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 
-function countUp (countBy, countTo) {
+var countUp = function(countTo, countBy) {
   var numberBank =[];
   for (var i = countBy; i <= countTo; i+=countBy) {
     numberBank.push(i);
   }
   return numberBank;
-}
+};
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var countTo = parseInt($("input#countTo").val());
+    var countBy = parseInt($("input#countBy").val());
+    //var result = countUp(countTo, countBy);
+    $(".result").text(countUp(countTo, countBy));
+
+    event.preventDefault();
+  });
+  //event.preventDefault();
+});
